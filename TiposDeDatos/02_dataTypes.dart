77ac43afcbbdbc4f5cadf54c60e6a4e9735799e4
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-09 11:55:09 -0600

    Strings

--- TiposDeDatos/02_dataTypes.dart
+++ TiposDeDatos/02_dataTypes.dart
@@ -1,15 +1,11 @@
 void main(List<String> args) {
   var a = 10;
   dynamic b = 5;
-
   int entero = 10;
   double decimal = 3.14;
-
   int _a = 30;
   double $b = 40;
-
   print(_a + $b);
-
   /*
 
   El null safety esta al tanto de no usar variables no definidas
@@ -22,4 +18,25 @@ void main(List<String> args) {
   print(c);
 
   */
+
+  // Strings ======================================================================================================================
+
+  var nombre1 = 'Tony';
+  String nombre2 = 'Stark';
+  String nombre3 = '0\'Connor';
+  String nombre4 = "0'Connor";
+
+  String nombreCompleto = '$nombre1 $nombre2';
+
+  String multilinea = '''
+  Hola Mundo
+  ¿Cómo estás? $nombreCompleto
+  O'Connor
+  ''';
+
+  // print(nombre1);
+  // print(nombre2);
+  // print(nombre3);
+  // print(nombre4);
+  print(multilinea);
 }
