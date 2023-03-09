f2e9dfe158d0625c265ef08773daefd8e47910b4
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-09 13:53:46 -0600

    Operadores relacionales y por tipo

--- 06_operadores_asignacion.dart
+++ 06_operadores_asignacion.dart
@@ -3,12 +3,25 @@ void main(List<String> args) {
   int? b;
 
   //b ??= 20; //Asigna el valor cuando la variable es null (Instrucción de seguridad)
-  print(b);
+  // print(b);
 
   int c = 28;
   String respuesta = c > 25 ? 'C es mayor a 25' : 'C es menor a 25'; // CONDICIÓN
-  print(respuesta);
+  // print(respuesta);
 
   int d = b ?? a ?? 100; // Checa si "b" es nulo, si si entonces pasa a "a", si sigue siendo nulo entonces pasa a "100"
-  print(d);
+  // print(d);
+
+  String persona1 = 'Leonardo';
+  String persona2 = 'Sairam';
+
+  // print(persona1 == persona2);
+  // print(persona1 != persona2);
+
+  //Operador de tipo
+  int i = 10;
+  String j = '10';
+
+  print(i is int);
+  print(j is String);
 }
