52f6613d419afa1ded3093ac95fb1c6ef6c2ceb7
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-09 16:46:14 -0600

    IF Else 2

--- 02_control/02_ifelse.dart
+++ 02_control/02_ifelse.dart
@@ -4,13 +4,13 @@ void main(List<String> args) {
   stdout.writeln('¿Cuántos años tienes?');
   int edad = int.parse(stdin.readLineSync()!);
 
-  if (edad < 18) {
-    stdout.writeln('menor de edad');
+  if (edad >= 21) {
+    stdout.writeln('ciudadano');
   } else {
-    if (edad > 20) {
-      stdout.writeln('ciudadano');
-    } else {
+    if (edad >= 18) {
       stdout.writeln('mayor de edad');
+    } else {
+      stdout.writeln('menor de edad');
     }
   }
 }
