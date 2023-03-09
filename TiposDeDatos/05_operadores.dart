768857d9fbd3bb0e5e9d20c2ce589e9313a5ea3f
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-09 13:36:18 -0600

    Operadores

---
+++ TiposDeDatos/05_operadores.dart
@@ -0,0 +1,23 @@
+/**
+ * Este archivo es para consultar los operadores
+ * Hecho por Leonardo Sáenz
+ */
+
+void main(List<String> args) {
+  int a = 10 + 5; // + 15
+  a = 20 - 10; // - 10
+  a = 10 * 2; // * 20
+
+  double b = 10 / 2; // / 5
+  b = 10.0 % 3; // % 1 Es el sobrante de la división.
+  b = -b; // -expr Es usado para negar el valor de la expresion.
+
+  int c = 10 ~/ 3; //Ctrl+Alt+Asterísco ~/ Es el resultado de la division pero entera.
+
+  int d = 1;
+  d++; //++ Incrementa en 1
+  d--; //-- Decrementa en 1
+
+  d += 2; // d = d + 2;
+  d -= 2; // d = d - 2;
+}
