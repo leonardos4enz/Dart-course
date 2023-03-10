bca4a7caecd0abe21d1d381238a280908e59fb2d
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-09 18:44:23 -0600

    Callback

---
+++ 03_funciones/05_callback.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  obtenerUsuario('100', (Map persona) {
+    print(persona);
+  });
+}
+
+void obtenerUsuario(String id, Function callback) {
+  Map usuario = {'id': id, 'nombre': 'Leonardo Sáenz'};
+
+  callback(usuario);
+}
