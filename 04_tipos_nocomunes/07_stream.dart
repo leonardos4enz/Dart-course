6cdeb7935f99a8eabe48098396e96d5d20bae035
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-10 00:02:15 -0600

    Streams - onError/onDone/cancelOnError

--- 04_tipos_nocomunes/07_stream.dart
+++ 04_tipos_nocomunes/07_stream.dart
@@ -3,10 +3,20 @@ import 'dart:async';
 void main(List<String> args) {
   final streamController = StreamController();
 
-  streamController.stream.listen((data) {
-    print("Despegando! $data");
-  });
+  streamController.stream.listen(
+    (data) => print("Despegando! $data"),
+    onError: (err) => print('Error! $err'),
+    onDone: () => print("¡Misión completa!"),
+    cancelOnError: false,
+  );
 
   streamController.sink.add('Apollo 11');
+  streamController.sink.add('Apollo 12');
+  streamController.sink.add('Apollo 13');
+  streamController.sink.addError('Houston, tenemos un problema.');
+  streamController.sink.add('Apollo 14');
+
+  streamController.sink.close();
+
   print('Fin del main()');
 }
