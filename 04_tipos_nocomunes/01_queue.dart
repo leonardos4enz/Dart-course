ee89deff84260f0a03689a1c2b9cbf9e4e0f2461
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-09 19:51:08 -0600

    Queue

---
+++ 04_tipos_nocomunes/01_queue.dart
@@ -0,0 +1,12 @@
+import 'dart:collection';
+
+void main(List<String> args) {
+  Queue<int> cola = new Queue();
+  cola.addAll([10, 20, 30, 40, 50]);
+  print(cola);
+
+  Iterator i = cola.iterator;
+  while (i.moveNext()) {
+    print(i.current);
+  }
+}
