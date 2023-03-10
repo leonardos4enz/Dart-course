304d09e681c41ace5bb48d99a9850f6788fe79f2
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-09 18:12:04 -0600

    Argumentos por nombre

--- 03_funciones/02_argumentos.dart
+++ 03_funciones/02_argumentos.dart
@@ -1,7 +1,13 @@
 saludar(String mensaje, [String nombre = '<Insertar nombre>']) {
+  // Mensaje obligatorio | nombre opcional
   print('$nombre: $mensaje');
 }
 
 void main(List<String> args) {
   saludar("Hola me llamo Leonardo");
+  saludar2(mensaje: 'Buenos días Nueva York', nombre: 'Leonardo');
+}
+
+saludar2({required String mensaje, required String nombre, int edad = 18}) {
+  print("Saludando: $mensaje, $nombre, $edad");
 }
