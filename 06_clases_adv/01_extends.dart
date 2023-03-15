9a559e404c332e0696aa8c73271a54d960859d8b
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-15 11:54:19 -0600

    Extends

---
+++ 06_clases_adv/01_extends.dart
@@ -0,0 +1,23 @@
+class Vehiculo {
+  bool encendido = false;
+
+  void encender() {
+    encendido = true;
+    print("Vehículo encendido!");
+  }
+
+  void apagar() {
+    encendido = false;
+    print("Vehículo apagado!");
+  }
+}
+
+class Carro extends Vehiculo {
+  int kilometraje = 0;
+}
+
+void main(List<String> args) {
+  final tesla = new Carro();
+
+  tesla.encender();
+}
