d5b70be4be452cd87380bf2ed9829ff3d77b72eb
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-15 18:59:12 -0600

    Mixings

---
+++ 06_clases_adv/05_mixings.dart
@@ -0,0 +1,39 @@
+mixin Logger {
+  void imprimir(String texto) {
+    final hoy = DateTime.now();
+
+    print("$hoy :::: $texto");
+  }
+}
+
+abstract class Astro with Logger {
+  String? nombre;
+
+  Astro() {
+    imprimir("Inicio de Astro");
+  }
+
+  void existo() {
+    print("Pienso, luego existo");
+  }
+}
+
+class Asteroide extends Astro {
+  String? nombre;
+
+  Asteroide(this.nombre) {
+    imprimir("Soy $nombre");
+  }
+}
+
+void main(List<String> args) {
+  final ceres = new Asteroide('Ceres');
+}
+
+
+
+/**
+ * No tienen constructores
+ * No se pueden inicializar
+ * No se puede extender (Se usan con la palabra "with")
+ */
