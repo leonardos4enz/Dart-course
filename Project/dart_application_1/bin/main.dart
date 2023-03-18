baa5dbffe7fb6d262294548d6684cd5217181c28
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-18 17:58:20 -0600

    Peticiones HTTP

--- Project/dart_application_1/bin/main.dart
+++ Project/dart_application_1/bin/main.dart
@@ -1,5 +1,10 @@
+import 'package:http/http.dart' as http;
+
 import 'package:dart_application_1/paquete_1.dart' as dart_application_1;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+void main(List<String> arguments) async {
+  final url = Uri.parse("https://reqres.in/api/users?page=2");
+  http.get(url).then((value) {
+    print(value);
+  });
 }
--- Project/dart_application_1/lib/paquete_1.dart
+++ Project/dart_application_1/lib/paquete_1.dart
@@ -1,9 +1,1 @@
-int calculate() {
-  int a = 10;
-  int b = 20;
 
-  a = 30;
-  b = 40;
-
-  return a * 10;
-}
