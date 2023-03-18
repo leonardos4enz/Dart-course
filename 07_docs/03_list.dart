c144461610f54877d9bde945739cc4dd27bfcaaa
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-18 13:29:33 -0600

    07_03_List2

--- 07_docs/03_list.dart
+++ 07_docs/03_list.dart
@@ -2,7 +2,7 @@ void main(List<String> args) {
   List<int> lista1 = [1, 2, 3, 4, 5];
   List<int> lista2;
   List<int> lista3 = [3, 1, 2, 15, -10];
-  List<String> nombre = ['Tony', 'Peter'];
+  List<String> nombres = ['Tony', 'Peter'];
 
   print("Lenght: ${lista1.length}");
   print("First: ${lista1[0]}");
@@ -12,4 +12,27 @@ void main(List<String> args) {
   print("is empty: ${lista1 == null}");
 
   print("asMap: ${lista1.asMap()[2]}");
+
+  print("indexOf: ${nombres.indexOf('Tony')}");
+
+  int mayor3 = lista1.indexWhere((element) => element > 3 ? true : false);
+  print(mayor3);
+
+  // print("Remove: ${nombres.remove('Tony')}");
+  // print(nombres);
+
+  lista1.shuffle();
+  print("Shuffle: $lista1");
+
+  lista3.sort();
+  print('Sort: $lista3');
+  print("Reverse: ${lista3.reversed.toList()}"); //Regresa un iterable asi que hay que ponerle el .toList()
+
+  nombres.forEach((nombre) {
+    nombre = nombre.toUpperCase();
+    print(nombre);
+  });
+
+  final newList = nombres.map((e) => e.toUpperCase());
+  print("newList = ${newList.toList()}");
 }
