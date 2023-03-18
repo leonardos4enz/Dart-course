d930dd6460606853fdced1b8cebde73bbb546593
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-18 15:37:54 -0600

    changePackageName

--- Project/dart_application_1/bin/main.dart
+++ Project/dart_application_1/bin/main.dart
@@ -1,4 +1,4 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/paquete_1.dart' as dart_application_1;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
--- Project/dart_application_1/test/dart_application_1_test.dart
+++ Project/dart_application_1/test/dart_application_1_test.dart
@@ -1,4 +1,4 @@
-import 'package:dart_application_1/dart_application_1.dart';
+import 'package:dart_application_1/paquete_1.dart';
 import 'package:test/test.dart';
 
 void main() {
