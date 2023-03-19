0c84df6bb66341b6777b11c93bb063de3daf3bb3
leonardos4enz <leonardorsaenz@gmail.com>
2023-03-18 19:03:42 -0600

    Optimizaciones del código anterior

--- Project/dart_application_1/bin/main.dart
+++ Project/dart_application_1/bin/main.dart
@@ -1,14 +1,8 @@
 import 'package:dart_application_1/clases/reqres_respuesta.dart';
-import 'package:http/http.dart' as http;
+import 'package:dart_application_1/serviceHttp.dart';
 
-import 'package:dart_application_1/paquete_1.dart' as dart_application_1;
+// import 'package:dart_application_1/paquete_1.dart' as dart_application_1;
 
 void main(List<String> arguments) async {
-  final url = Uri.parse("https://reqres.in/api/users?page=2");
-  http.get(url).then((res) {
-    final resReqRes = reqResRespuestaFromJson(res.body);
-    print('page: ${resReqRes.page}');
-    print('per_page: ${resReqRes.perPage}');
-    print('id del tercer elemento: ${resReqRes.data[2].id}');
-  });
+  getReqRes_service();
 }
